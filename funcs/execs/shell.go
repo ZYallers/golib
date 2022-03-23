1c7178fccc812a219f6d673e8c57e3a4a14ae50e
ZYaller <zyb_icanplay@163.com>
2022-03-23 14:47:20 +0800

    Optimize funcs directory;

--- funcs/execs/shell.go
+++ funcs/execs/shell.go
@@ -1,4 +1,4 @@
-package funcs
+package execs
 
 import (
 	"bytes"
@@ -6,7 +6,7 @@ import (
 )
 
 // 阻塞式的执行外部shell命令的函数, 等待执行完毕并返回标准输出
-func ExecShell(name string, arg ...string) ([]byte, error) {
+func Shell(name string, arg ...string) ([]byte, error) {
 	// 函数返回一个*Cmd，用于使用给出的参数执行name指定的程序
 	cmd := exec.Command(name, arg...)
 
--- funcs/nets/ip.go
+++ funcs/nets/ip.go
@@ -1,4 +1,4 @@
-package funcs
+package nets
 
 import (
 	"encoding/json"
