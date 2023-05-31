a7df0631204b38962dfad731d5ebf05ea46bbc0b
ZYaller <zyb_icanplay@163.com>
2023-05-31 19:15:22 +0800

    Change zap new log add cache quantity control

--- utils/logger/zap_test.go
+++ utils/logger/zap_test.go
@@ -2,14 +2,16 @@ package logger
 
 import (
 	"go.uber.org/zap"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"sync"
 	"testing"
+	"time"
 )
 
 func Test_Zap_Use(t *testing.T) {
-	SetLoggerDir("/Users/cloud/projects/ZYallers/golib")
+	SetLoggerDir("./test_log")
 	Use("ddd").Info("1234")
 	var wg sync.WaitGroup
 	for i := 0; i < 100; i++ {
@@ -19,6 +21,30 @@ func Test_Zap_Use(t *testing.T) {
 			is := strconv.Itoa(i)
 			Use("").Error(strings.Repeat(is, 5))
 			Use("eeee").Info(is, zap.Any("len", loggerDict.Len()))
+		}(i)
+	}
+	wg.Wait()
+}
+
+func Test_Zap_NewLogger(t *testing.T) {
+	fileName := func(fn, dir string) string {
+		if fn == "" {
+			fn = time.Now().Format("20060102")
+		}
+		if dir == "" {
+			dir, _ = filepath.Abs(filepath.Dir("."))
+		}
+		fp, _ := filepath.Abs(dir + "/" + fn + suffix)
+		return fp
+	}
+	var wg sync.WaitGroup
+	for i := 0; i < 52; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			is := strconv.Itoa(i)
+			NewLogger(fileName("test@"+is, "./test_log")).Info(is)
+			NewLogger(fileName("record", "")).Debug(is, zap.Int("len", loggerDict.Len()))
 		}(i)
 	}
 	wg.Wait()
